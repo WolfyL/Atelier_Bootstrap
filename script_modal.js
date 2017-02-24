f34d7eb1bb467b32f59d10883678ddc7ac987403
Wolfy <s.leheup@gmail.com>
2017-02-24 18:36:05 +0100

    Added pic and used modal + fixed my issue to center the button containing pic with col-xs (in smartphone screen)

--- script_modal.js
+++ script_modal.js
@@ -18,3 +18,24 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modal.style.display = "none";
 }
+
+//
+// //  <!-- Get the modal -->
+// var modal2 = document.getElementById('myModal2');
+// //  <!-- Get the image and insert it inside the modal - use its "alt" text as a caption -->
+// var img2 = document.getElementById('myImg2');
+// var modalImg2 = document.getElementById("img02");
+// var captionText2 = document.getElementById("caption2");
+// img.onclick = function(){
+//     modal2.style.display = "block";
+//     modalImg2.src = this.src;
+//     captionText2.innerHTML = this.alt;
+// }
+//
+// //  <!-- Get the <span> element that closes the modal -->
+// var span2 = document.getElementsByClassName("close")[0];
+//
+// //  <!-- When the user clicks on <span> (x), close the modal -->
+// span2.onclick = function() {
+//     modal2.style.display = "none";
+// }
